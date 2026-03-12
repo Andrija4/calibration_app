a2cd37ddf520f9a439b641db515098effb37efd3
Andrija4 <a.ilicc2002@gmail.com>
2026-03-12 14:02:51 +0100

    refactor(database): Handeling multiple employees to use the app at the same time

--- app/database.py
+++ app/database.py
@@ -5,12 +5,28 @@ DATABASE_EQUIPMENT_URL = "sqlite:///./calibration.db"
 DATABASE_MAIL_URL = "sqlite:///./mailing.db"
 
 engine_equipment = create_engine(
-    DATABASE_EQUIPMENT_URL, connect_args={"check_same_thread": False}
+    DATABASE_EQUIPMENT_URL, connect_args={"check_same_thread": False}, pool_pre_ping=True
 )
 engine_mail = create_engine(
-    DATABASE_MAIL_URL, connect_args={"check_same_thread": False}
+    DATABASE_MAIL_URL, connect_args={"check_same_thread": False}, pool_pre_ping=True
 )
 
 SessionLocal = sessionmaker(bind=engine_equipment)
 SessionLocalMail = sessionmaker(bind=engine_mail)
 Base = declarative_base()
+
+from sqlalchemy import event
+
+@event.listens_for(engine_equipment, "connect")
+def set_sqlite_pragma(dbapi_connection, connection_record):
+    cursor = dbapi_connection.cursor()
+    cursor.execute("PRAGMA journal_mode=WAL")
+    cursor.execute("PRAGMA synchronous=NORMAL")
+    cursor.close()
+
+@event.listens_for(engine_mail, "connect")
+def set_sqlite_pragma_mail(dbapi_connection, connection_record):
+    cursor = dbapi_connection.cursor()
+    cursor.execute("PRAGMA journal_mode=WAL")
+    cursor.execute("PRAGMA synchronous=NORMAL")
+    cursor.close()
